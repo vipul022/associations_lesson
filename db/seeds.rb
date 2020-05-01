ecd8a73802e93b1f574d64d635b227af6b46903c
vipul022 <vipul.seth022@gmail.com>
2020-05-01 13:40:43 +1000

    seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-10.times do
-  Author.create(
+authors = []
+for i in 1..10
+  author = Author.create(
     name: Faker::Book.author,
     date_of_birth: Faker::Date.birthday(18, 65)
   )
+  authors.push(author)
+  puts "Created #{i} authors"
+end
+
+for i in 1..50
+    Book.create(
+      title: Faker::Book.title,
+      author_id: authors[rand(0..9)].id
+    )
+    puts "Created #{i} books"
+end
+
+genres = ["fantasy", "horror", "romance", "crime", "sci-fi"]
+
+for i in genres
+    Genre.create(name: i)
+    puts "Created #{i} genre"
 end
